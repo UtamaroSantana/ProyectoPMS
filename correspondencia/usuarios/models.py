dd7b4a6d11fa42abc5840dcb00ff92c5dfdc909c
UtamaroSantana <37180283@uaz.edu.mx>
2022-12-05 23:27:35 -0600

    Pruebas

--- correspondencia/usuarios/models.py
+++ correspondencia/usuarios/models.py
@@ -15,7 +15,7 @@ class Usuario(User):
         return self.last_name + ' ' + self.first_name
 
 class Ajustes(models.Model):
-    titulo = models.CharField(max_length=100, validators=[valor_minimo_titulo,solo_Alphabeto], verbose_name="Título", default="Sistema de Control y Seguimiento de Correspondencias")
+    titulo = models.CharField(max_length=100, validators=[valor_minimo_titulo], verbose_name="Título", default="Sistema de Control y Seguimiento de Correspondencias")
     subtitulo = models.CharField(max_length=100, validators=[valor_minimo_subtitulo,solo_Alphabeto], verbose_name="Subtitulo", default="LABSOL")
     logo = models.ImageField("Logo de institución",upload_to="logo/", blank=True, null=True)
 
